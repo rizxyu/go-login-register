ccbfc7c22c0f415c87ebfbb943b7f682da529663
Vania Alexa <96773317+fckvania@users.noreply.github.com>
2022-06-28 15:11:55 +0700

    update users.go

--- models/users.go
+++ models/users.go
@@ -1,20 +1,5 @@
 package models
 
-import (
-	"github.com/jinzhu/gorm"
-)
-
-type SignupForm struct {
-	Username string `scheme:"username"`
-	Email    string `scheme:"email"`
-	Password string `scheme:"password"`
-}
-
-type SigninForm struct {
-	Email    string `scheme:"email"`
-	Password string `scheme:"password"`
-}
-
 type User struct {
 	gorm.Model
 	ID       uint64 `gorm:"primary_key:auto_increment" json:"id" sql:"id"`
